7d7f4008984d963e5e20b25d959e3fa7ea059966
Mr. Blue <provaprove3@gmail.com>
2024-03-31 23:07:13 +0200

    Updated closeTicket function
    Updated the closeTicket function in utilis/ticketManager to prevent the function to attmept to reclose an already closed ticket. A new error was created for this particular case in errors.yml.

--- utilis/ticketManager/closeTicket.js
+++ utilis/ticketManager/closeTicket.js
@@ -1,4 +1,11 @@
+const sendErrorEmbed = require("../sendErrorEmbed");
+
 module.exports = async (channel, interaction) => {
+    if(channel.parentId == interaction.client.tickets.closed_category_id) {
+        sendErrorEmbed(interaction.client, interaction, "closed_ticket");
+        return;
+    }
+
     const newChannelName = interaction.client.tickets.closed_emoji + channel.name;
 
     await channel.setName(newChannelName);
